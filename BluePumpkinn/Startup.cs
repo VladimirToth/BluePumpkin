64a7a6cf986257cae6428ffdce15640ce5c3ffb8
VladimirToth <straukar@gmail.com>
2015-11-04 19:18:40 +0100

    FirstCommit

---
+++ BluePumpkinn/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BluePumpkinn.Startup))]
+namespace BluePumpkinn
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
