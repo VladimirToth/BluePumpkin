98b00a75320f74711a8f43e5e18f03e755cc3187
VladoToth <straukar@gmail.com>
2015-11-17 20:07:46 +0100

    Add OAuth for Google, Microsoft, Facebook

--- BluePumpkinn/Controllers/HomeController.cs
+++ BluePumpkinn/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ namespace BluePumpkinn.Controllers
 {
     public class HomeController : Controller
     {
+        [RequireHttps]
         public ActionResult Index()
         {
             return View();
